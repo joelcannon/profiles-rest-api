2b0897a2c0397d0d12c341492b1a4d9125e773e5
Joel Cannon <joelcannon@mac.com>
2021-02-23 21:21:55 -0700

    Added allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '#gnmi+=b)=7&=w%4bqw0q!v_+8di2&s-y@ub86tud9nuu#6drj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-18-237-40-145.us-west-2.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
